8e0c6edc778c933578d04491917be68a777baf25
Domin <hyungm3@gmail.com>
2020-04-09 19:15:05 +0900

    megait

---
+++ MegaIT/my_module.py
@@ -0,0 +1,15 @@
+# 입력값 X, 반환값 X
+def my_fun1():
+    print("hello")
+# 입력값 O, 반환값 X
+def my_fun2(name):
+    print(name, "hello")
+# 입력값 X, 반환값 O
+def my_fun3():
+    x = 10
+    return x
+# 입력값 O, 반환값 O
+def my_fun4(a, b):
+    return a * b
+
+test = 10
